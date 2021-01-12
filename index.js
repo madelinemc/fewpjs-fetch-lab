b7c8b054bb0d6fe04505b959a9a1f5602d36a86b
madelinemc <madeline.coons@gmail.com>
2021-01-12 14:24:49 -0600

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-function fetchBooks() {
-
+function fetchBooks() { 
+return fetch('https://anapioficeandfire.com/api/books') //fetchBooks() should return the fetch()
+.then(function(response) {
+  return response.json(); //the returned response should be converted to JSON 
+})
+.then(function(json) { //then call renderBooks() passing in the API's returned JSON as the argument
+  renderBooks(json);
+})
 }
 
 function renderBooks(books) {
